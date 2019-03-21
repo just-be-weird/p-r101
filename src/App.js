3de3efbc4b49ae77880b8596319a8fc38770c8d0
abhishek <abhishek.prajapati@adapty.com>
2019-03-21 13:52:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'in h1'));
+  }
+}
+
+export default App;
