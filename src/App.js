1b7ec8c91de8e4813bf3bf7fcbd7adbaf76ba73d
just-be-weird <abhi.chandresh@gmail.com>
2019-03-23 17:16:48 +0530

    Module-3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    person: [
+      {name: 'Jan', age: 20},
+      {name: 'April', age: 24}
+    ]
+  }
+
+  switchNameHandler = ()=> {
+    this.setState({
+      person: [
+        {name: 'June' , age: 24},
+        {name: 'April', age: 26}
+      ]
+    })
+  }
+
+  nameChangeHandler = (event)=> {
+    this.setState({
+      person: [
+        {name: 'June' , age: 24},
+        {name: event.target.value , age: 26}
+      ]
+    })
+  }
+  
+
   render() {
-    return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'in h1'));
+    return (
+      <div className="App">
+        <h1>Understanding React Syntax</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.person[0].name} switchNameHandler={this.switchNameHandler} age={this.state.person[0].age}> children properties </Person>
+        <Person  name={this.state.person[1].name} changed={this.nameChangeHandler} age={this.state.person[1].age}/>
+      </div>
+    );
   }
 }
 
